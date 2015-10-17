e97d762be8d0c878627892bd69dbf8d42d5c161f
Rafael Cano Palomino <desarrollo@rafacano.es>
2015-10-17 19:40:27 +0200

    Arreglo cookies
    Arrelgo las cookies

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,27 +1,10 @@
-    app.get("/", function (req, res) {
-        
-        // if (!req.isAuthenticated()) {
-            // página de index cuando no login.
-        res.render('index.ejs'
-            // {
-            //     layout: 'layout',
-            //     title: 'Visitas',
-            //     user: null,
-            //     errors: []
-            // }
-            )
-        // } else {
-        //     // página de index cuando no login.
-        //     res.redirect('/visitas');
-        // }
-/* global nomApp */
 'use strict';
 module.exports = function (app, passport) {
   app.get('/', function (req, res) {
     res.render('index.ejs', {
-        title: app.get('nomApp'),
-        errors: []
-      });
-  });
+      title: app.get('nomApp'),
+      errors: []
     });
+  });
 };
+  
--- public/javascripts/cookies.js
+++ public/javascripts/cookies.js
@@ -1,6 +1,6 @@
 // Script para control de política de cookies. 
 // La pagina tiene qut tener un elemento div id="cookie1", para que funcione correctamente.
-if (localStorage.controlCookie && localStorage.controlCookie > 0)
+if (localStorage.controlCookie && !isNaN(localStorage.controlCookie) && parseInt(localStorage.controlCookie) > 0) 
   cookie1.style.display='none';
 else
   if (location.pathname != "/" && location.pathname != "/politica.html")
@@ -8,9 +8,11 @@ else
 
 function controlCookies() {
   // si variable no existe se crea (al clicar en Aceptar)
-  localStorage.controlCookie = (localStorage.controlCookie || 0);
-  if (localStorage.controlCookie == 0) {
-    localStorage.controlCookie++; // incrementamos cuenta de la cookie
+  localStorage.controlCookie = (localStorage.controlCookie || "0");
+  if (isNaN(localStorage.controlCookie) || parseInt(localStorage.controlCookie)== 0) {
+    localStorage.controlCookie="1"; // incrementamos cuenta de la cookie
+  }
+  if (!isNaN(localStorage.controlCookie) && parseInt(localStorage.controlCookie) > 0) {
     cookie1.style.display='none'; // Esconde la política de cookies
   }
 }
